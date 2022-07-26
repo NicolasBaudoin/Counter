df686a99f741f04996d101dffbc74afca11bc92e
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2022-07-26 16:00:39 +0200

    separate the condition statement to have text in red only for negative numbers, black only for 0 and green only for positive numbers

--- main.js
+++ main.js
@@ -5,20 +5,27 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn"); //nodelist
 const span = document.querySelector("span");
-//for each on nodelist
+
+//for each on nodelist to increase, decrease or reset the count
+
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
     if (styles.contains("decrease")) {
       count--;
-      span.style.color = "red";
     } else if (styles.contains("increase")) {
       count++;
-      span.style.color = "green";
     } else if (styles.contains("reset")) {
       count = 0;
-      span.style.color = "black";
     }
     value.textContent = count;
+    // change caracters color according to count
+    if (value.textContent < 0) {
+      span.style.color = "red";
+    } else if (value.textContent == 0) {
+      span.style.color = "black";
+    } else if (value.textContent > 0) {
+      span.style.color = "green";
+    }
   });
 });
