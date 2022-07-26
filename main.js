542c86e327aa98c843e121e9688415c3bce9356b
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2022-07-26 15:48:51 +0200

    add a conditional statement to change caracters colors in red if it's lower than 0, in black if it's 0, in green if more than 0

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let count = 0;
 //select value and btn
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn"); //nodelist
-
+const span = document.querySelector("span");
 //for each on nodelist
 
 btns.forEach(function (btn) {
@@ -18,5 +18,12 @@ btns.forEach(function (btn) {
       count = 0;
     }
     value.textContent = count;
+    if (value.textContent < 0) {
+      span.style.color = "red";
+    } else if (value.textContent == 0) {
+      span.style.color = "black";
+    } else if (value.textContent > 0) {
+      span.style.color = "green";
+    }
   });
 });
