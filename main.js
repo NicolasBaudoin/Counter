d5b356aa9f93c807d399eb6d48bf34aac222a381
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2022-07-26 03:19:51 +0200

    select all btn, use a for each on a nodelist, function with currenttarget element

---
+++ main.js
@@ -0,0 +1,14 @@
+//initial count
+let count = 0;
+
+//select value and btn
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn"); //nodelist
+
+//for each on nodelist
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    console.log(e.currentTarget);
+  });
+});
